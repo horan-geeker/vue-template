301a9a1883e12533035cfc1fff2445218fabf676
horan <13571899655@163.com>
2025-12-21 00:01:43 +0800

    完善前端模版

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,16 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    Sitemap({
+      hostname: 'https://example.com', // Update with actual domain
+    }),
   ],
   resolve: {
     alias: {
